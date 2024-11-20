3fb27cd9897aa04cbc908410e4e8a0ae928eb64a
Edmond Prin <edmondprin@gmail.com>
2024-11-20 08:56:46 -0600

    Added speedfunction folder

---
+++ math_date/math_date.js
@@ -0,0 +1,14 @@
+const number = 3.6;
+const rounded = Math.round(number) // Round to nearest integer: 4
+const ceil = Math.cell(number); // Round up: 4
+const floor = Math.floor(number); // Round down: 3;
+
+const base = 2;
+const exponent = 3;
+
+const power = Math.pow(base, exponent); // Power: 8
+const squareRoot = Math.sqrt(base); // Square Root: 1.41421356237
+const naturalLog = Math.log(base); // Natural Logarithm: 0.69314718056
+
+const random = Math.random(); // Generates a random # between 0 and 1 including decimal values;
+
---
+++ math_date/quotes.js
@@ -0,0 +1,41 @@
+let quotes = [
+    "Life is what happens when you're busy making other plans. - John Lennon",
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "In three words, I can sum up everythin I've learned about life: It goes on. - Robert Frost",
+    "Don't count the days, make the days count. - Muhammad Ali",
+    "The only thing we have to fear is fear itself. - Franklin D. Roosevelt",
+    "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment. - Ralph Waldo Emerson"
+];
+
+// function generateRandomQuote() {
+//     const randomIndex = Math.floor(Math.random() * quotes.length);
+//     // Generate a random index
+//     const randomQuote = quotes[randomIndex]; // Get a random quote
+
+//     document.getElementById("quoteDisplay").textContent = randomQuote;
+// }
+
+
+
+// let randomQuote;
+// function generateRandomQuote() {
+//     for (let i=0; i<quotes.length; i++) {
+//         const randomIndex = Math.floor(Math.random() * quotes.length);
+//         randomQuote = quotes[randomIndex];
+//         document.getElementById("quoteDisplay").textContent = randomQuote;
+//         // quotes.pop(randomQuote[i]);
+        
+//     }
+    
+// }
+    
+
+// quotes.forEach((quote) => {
+//     console.log(quote);
+// })
+
+// for (let i=0; i<quotes.length; i++) {
+//     console.log(`quote ${i+1}: ${quotes[i]}`);
+//     quotes.shift(quotes[i]);
+//     console.log(quotes);
+// }
